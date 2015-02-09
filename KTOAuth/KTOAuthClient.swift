915f43b3b636f0f4b88d3c56e03b719d7f258dd4
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2015-02-09 08:45:53 +0100

    Added the initial KTOAuthClient which takes a delegate that implements the KTOAuthClientProtocol.

---
+++ KTOAuth/KTOAuthClient.swift
@@ -0,0 +1,15 @@
+//
+//  KTOAuthClient.swift
+//  KTOAuth
+//
+//  Created by Kevin Tuhumury on 08/02/15.
+//  Copyright (c) 2015 Kevin Tuhumury. All rights reserved.
+//
+
+import Foundation
+
+class KTOAuthClient {
+
+  var delegate: KTOAuthClientProtocol?
+
+}
