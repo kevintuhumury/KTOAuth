d9f2c58378d9503f1bd48819fe161a21699afca6
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2015-02-11 22:37:13 +0100

    Added the init() method which takes the required parameters and sets up the request forgery state.

--- KTOAuth/KTOAuthClient.swift
+++ KTOAuth/KTOAuthClient.swift
@@ -12,6 +12,24 @@ class KTOAuthClient {
 
   var delegate: KTOAuthClientDelegate?
 
+  var clientId: String
+  var clientSecret: String
+  var redirectURI: String
+  var authorizeURL: String
+  var tokenURL: String
+
+  var requestForgeryState: String?
+
+  init(clientId: String, clientSecret: String, redirectURI: String, authorizeURL: String, tokenURL: String) {
+    self.clientId     = clientId
+    self.clientSecret = clientSecret
+    self.redirectURI  = redirectURI
+    self.authorizeURL = authorizeURL
+    self.tokenURL     = tokenURL
+
+    requestForgeryState = encodedRequestForgeryState()
+  }
+
   private func encodedRequestForgeryState() -> String {
     return (NSUUID().UUIDString).dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)!.base64EncodedStringWithOptions(nil)
   }
